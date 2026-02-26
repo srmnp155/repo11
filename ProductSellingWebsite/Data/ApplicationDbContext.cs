3ef2789f09a737c38b03011a8d72be559b183b92
srmnp155 <srmnp155@gmail.com>
2026-02-26 13:39:39 +0000

    Generated code via AI

---
+++ ProductSellingWebsite/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ProductSellingWebsite.Models;
+
+namespace ProductSellingWebsite.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
